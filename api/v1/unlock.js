3f5c9b64ddec3a745cdcf28d66998fdbb5cb5a09
Dillon Corkran <dcorkran972@gmail.com>
2017-02-10 12:45:47 -0700

    :sparkles: server configured to send get request to pi

---
+++ api/v1/unlock.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+var rp = require('request-promise');
+const PI_URL = 'http://10.6.64.164:3000';
+
+router.get('/unlock', function(req, res, next) {
+
+  var options = {
+    uri: `${PI_URL}/unlock`,
+    json: true // Automatically parses the JSON string in the response
+  };
+
+
+  rp(options)
+      .then(function (data) {
+        console.log(data);
+        res.json(data);
+      })
+      .catch(function (err) {
+        console.log(err);
+        res.json(err);
+          // API call failed...
+      });
+});
+
+module.exports = router;
