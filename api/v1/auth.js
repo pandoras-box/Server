3d5f0f10427503166cdfd6da5a746a20110121e3
Dillon Corkran <dcorkran972@gmail.com>
2017-02-14 11:36:23 -0700

    :sparkles: auth

--- api/v1/auth.js
+++ api/v1/auth.js
@@ -1,10 +1,23 @@
 var express = require('express');
 var router = express.Router();
 const dbQueries = require('../../db/queries');
+const fbAuth = require ('../../auth/facebook')
 
 router.post('/', function(req, res, next) {
-  console.log(req.body);
-  res.json(true);
+  // add logic to check local storage for a Long term token
+  //get the token and make sure it is facebook valid
+  //if valid, give the user a JWT with their userID
+  //if it is not valid, make them sign in again
+  //if it does not exist, add them to the db and give a token
+
+  fbAuth.getFBShortAccess(req.body.token)
+  .then(function (result) {
+    console.log(result);
+  })
+  .catch(function (err) {
+    console.log(err);
+    res.json("boo");
+  });
 });
 
 module.exports = router;
---
+++ auth/facebook.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var rp = require('request-promise');
+const FACEBOOK_URL = 'https://graph.facebook.com';
+const LOCAL_URL = 'http://localhost/callback';
+const dotenv = require('dotenv').config();
+
+
+
+module.exports = {
+  getFBShortAccess: function(access_token){
+    console.log(access_token);
+    var options = {
+      method: 'GET',
+      uri: `${FACEBOOK_URL}/v2.2/me?fields=email,name,gender,picture&access_token=${access_token}`,
+    };
+    return rp(options);
+  }
+
+
+}
