006e097be230c715fc01a9e90fe3d6ff3877f23f
Dillon Corkran <dcorkran972@gmail.com>
2017-02-14 19:05:32 -0700

    :+1: login with facebook complete

--- seeds/04_batch.js
+++ seeds/04_batch.js
@@ -1,9 +1,13 @@
 exports.seed = function(knex, Promise) {
-  return knex.raw('DELETE FROM "batch"; ALTER SEQUENCE batch_id_seq RESTART WITH 2')
+  return knex.raw('DELETE FROM "batch"; ALTER SEQUENCE batch_id_seq RESTART WITH 3')
   .then(()=>{
     return Promise.all([
       knex("batch").insert([{
         id:1,
+        'created_date':'2016-02-14 12:00:00',
+        'due_date':'2016-02-07 11:00:00'
+      },{
+        id:2,
         'created_date':'2016-02-14 12:00:00',
         'due_date':'2016-02-07 11:00:00'
       }])
--- seeds/05_parent_child.js
+++ seeds/05_parent_child.js
@@ -1,11 +1,15 @@
 exports.seed = function(knex, Promise) {
-  return knex.raw('DELETE FROM "parent_child"; ALTER SEQUENCE parent_child_id_seq RESTART WITH 2')
+  return knex.raw('DELETE FROM "parent_child"; ALTER SEQUENCE parent_child_id_seq RESTART WITH 3')
   .then(()=>{
     return Promise.all([
       knex("parent_child").insert([{
         id:1,
         'parent_id':1,
         'child_id':1
+      },{
+        id:2,
+        'parent_id':1,
+        'child_id':2
       }])
     ]);
   });
