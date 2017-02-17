9301af5f80e0cdd925453a2261631dea3a703581
Dillon Corkran <dcorkran972@gmail.com>
2017-02-16 19:18:24 -0700

    :+1: more events

--- seeds/03_event.js
+++ seeds/03_event.js
@@ -1,5 +1,5 @@
 exports.seed = function(knex, Promise) {
-  return knex.raw('DELETE FROM "event"; ALTER SEQUENCE event_id_seq RESTART WITH 4')
+  return knex.raw('DELETE FROM "event"; ALTER SEQUENCE event_id_seq RESTART WITH 8')
   .then(()=>{
     return Promise.all([
       knex("event").insert([{
@@ -11,6 +11,18 @@ exports.seed = function(knex, Promise) {
       },{
         id:3,
         'category':'Clean Dishes'
+      },{
+        id:4,
+        'category':'Do Homework'
+      },{
+        id:5,
+        'category':'Practice Instrument'
+      },{
+        id:6,
+        'category':'Play Outside'
+      },{
+        id:7,
+        'category':'Call Grandma'
       }])
     ]);
   });
